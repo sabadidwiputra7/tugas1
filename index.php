06b2afcfb8e43735da22dbaa9165c0cc63e0cb7f
iamo <50961295+sabadidwiputra7@users.noreply.github.com>
2019-10-12 09:04:06 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<style>
+	div.a {
+	position: absolute;
+	margin-top: -100px;
+	margin-left: -200px;
+	left: 50%;
+	top: 50%;
+	width: 400px;
+	height: 220px;
+}
+</style>
+
+<div class ="a">
+<?php
+	
+	echo "<body style='background-color:gold'>";
+	echo "<table border = '2' >";
+	echo "<tr>";
+	echo "<td><font size ='14'> NIM </td>" ;
+	echo "<td><font size ='14'> 10116314 </td> " ;
+	echo "</tr>";
+	echo "<tr>";
+	echo "<td><font size ='14'> Nama </td>"; 
+	echo "<td><font size ='14'> SABADI DWIPUTRA </font> </td> " ;
+	echo "</tr>";
+	echo "</table>";
+?>
+
+</div>
+	
